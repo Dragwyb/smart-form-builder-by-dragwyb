e85727fc8d3e7bcbbb7893a861b7b5ce72eebe21
dragwyb <dragwyb@gmail.com>
2025-08-29 00:48:14 +0530

    Added form title & status in label field.
    Added form title & status in label field.

--- assets/src/editor/Editor/header/index.jsx
+++ assets/src/editor/Editor/header/index.jsx
@@ -1,12 +1,12 @@
-import { useSelector,  useStore, useDispatch } from 'react-redux';
+import { useSelector, useStore, useDispatch } from 'react-redux';
 import { Utils as Helper } from '../../components/Utils';
 import { Button, SaveBtn } from '../../components/Common';
 import { __ } from '@wordpress/i18n';
 
 
 const Header = () => {
-    const formTitle=useSelector(state => state?.advance?.form_title || DragwybEditor.formData.title); 
-    const formStatus = useSelector(state => state?.advance?.form_status || DragwybEditor.formData.status);
+    const formTitle = useSelector(state => state?.form?.advance?.form_name || DragwybEditor.formData.title);
+    const formStatus = useSelector(state => state?.form?.advance?.form_status || DragwybEditor.formData.status);
     const previewMode = useSelector(state => state.previewMode);
 
     const dispatch = useDispatch();
@@ -20,18 +20,26 @@ const Header = () => {
         window.location.href = DragwybEditor.adminUrl;
     };
 
+    const setActiveTabHandler = (value) => {
+        Utils.setSelectedSettingId({ value: false });
+        Utils.setActiveTab({ value: value });
+        Utils.setPreviewMode({ value: false });
+    }
+
     return <div className="dragwyb-editor__header">
         <div className="dragwyb-editor__details">
             <h2>Dragwyb Form Builder</h2>
-            <div className="dragwyb-editor__title">
-                <p>
-                    {formTitle}
-                </p>
-            </div>
-            <div className="dragwyb-editor__status" data-status={formStatus}>
+            <div className="dragwyb-editor__status" data-status={formStatus} onClick={()=>setActiveTabHandler('advance')}>
+                <label for="form_status">
                 <span data-status={formStatus}></span>
                 <p>{formStatus.charAt(0).toUpperCase() + formStatus.slice(1)}</p>
+                </label>
             </div>
+        </div>
+        <div className="dragwyb-editor__title" onClick={()=>setActiveTabHandler('advance')}>
+            <h2>
+                <label for="form_name">{formTitle}</label>
+            </h2>
         </div>
         <div className="dragwyb-editor__actions">
             <Button onClick={() => Utils.setPreviewMode({ value: !previewMode })} className='dragwyb-preview'>
