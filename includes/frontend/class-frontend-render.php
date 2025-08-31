04678c15d920f70dccff80747cbda05507d18844
dragwyb <dragwyb@gmail.com>
2025-08-31 23:53:53 +0530

    Fixed frontend render issue after adding toolbars.
    Fixed frontend render issue after adding toolbars.

--- includes/frontend/class-frontend-render.php
+++ includes/frontend/class-frontend-render.php
@@ -8,6 +8,8 @@ use Dragwyb\Form_Builder\Includes\Controls\Controls;
 use Dragwyb\Form_Builder\Includes\Modules\Modules;
 use Dragwyb\Form_Builder\Includes\Modules\Fields\Field_Base;
 use Dragwyb\Form_Builder\Includes\Controls\Controls\Control_Base;
+use Dragwyb\Form_Builder\Includes\Toolbars\Toolbars;
+use Dragwyb\Form_Builder\Includes\Toolbars\Toolbar_Base;
 
 if (!defined('ABSPATH')) {
     exit;
@@ -15,8 +17,8 @@ if (!defined('ABSPATH')) {
 
 class Frontend_Render
 {
-    private $form_id;
-    private $fields;
+    private static $form_id;
+    private static $fields = array();
 
     private static $control = null;
     private static $module = null;
@@ -24,14 +26,24 @@ class Frontend_Render
     private static $field_module_cache = null;
 
     private static $Field_Data = null;
+    private static $form_data = null;
+    private static $toolbar_data = array();
 
     public function __construct($form_id)
     {
-        $this->form_id = $form_id;
-        $this->fields = get_post_meta($form_id, '_dragwyb_form_fields', true);
+        self::$form_id = $form_id;
+        self::$toolbar_data = array();
+        self::$fields = array();
+        self::$form_data = get_post_meta($form_id, '_dragwyb_form_data', true);
+
+        if (empty(self::$form_data) || !is_array(self::$form_data) || !isset(self::$form_data['fields']) || count(self::$form_data) < 1) {
+            self::$fields = array();
+            return;
+        }
 
         $this->set_control();
         $this->set_module();
+        $this->set_toolbar_data();
     }
 
     private function set_module(): void
@@ -44,10 +56,35 @@ class Frontend_Render
         self::$control =  new Controls();
     }
 
+    private function set_toolbar_data(): void
+    {
+        $toolbar_obj = new Toolbars();
+        $toolbars = $toolbar_obj->get_toolbars();
+
+        foreach (self::$form_data as $key => $value) {
+            if ($key === 'id') {
+                continue;
+            }
+
+            if (count($toolbars) > 0 && isset($toolbars[$key]) && $toolbars[$key] instanceof Toolbar_Base) {
+                $toolbar = $toolbars[$key];
+                $toolbar->set_form_id(self::$form_id);
+                $toolbar->set_toolbar_data($value);
+                $toolbar_data = $toolbar->get_toolbar_data();
+
+                if ($toolbar_data) {
+                    if ($key === 'fields') {
+                        self::$fields = $toolbar_data;
+                    } else {
+                        self::$toolbar_data[$key] = $toolbar_data;
+                    }
+                }
+            }
+        }
+    }
+
     public function render(): string
     {
-        if (!$this->fields || !is_array($this->fields) || count($this->fields) < 1) return '';
-
         return $this->render_fields();
     }
 
@@ -56,9 +93,14 @@ class Frontend_Render
 
         ob_start();
 
-        echo '<form class="dragwyb-form" id="dragwyb-form-' . esc_attr($this->form_id) . '">';
+        if (count(self::$fields) < 1) {
+            echo '<p>' . esc_html__('No fields found in this form.', 'dragwyb-form-builder') . '</p>';
+            return ob_get_clean();
+        }
 
-        foreach ($this->fields as $field) {
+        echo '<form class="dragwyb-form" id="dragwyb-form-' . esc_attr(self::$form_id) . '">';
+
+        foreach (self::$fields as $field) {
             if (!isset($field['_id']) || !$field['type'] || empty($field['_id']) || empty($field['type'])) {
                 continue;
             }
