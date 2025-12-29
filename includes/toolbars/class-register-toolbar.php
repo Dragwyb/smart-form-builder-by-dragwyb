f6af1d5de3311ed834c8e74caa378c319e4c8da5
dragwyb <dragwyb@gmail.com>
2025-12-29 22:49:27 +0530

    Update default toolbars.
    Update default toolbars.

--- includes/toolbars/class-register-toolbar.php
+++ includes/toolbars/class-register-toolbar.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Dragwyb\Form_Builder\Includes\Toolbars;
@@ -6,7 +7,8 @@ namespace Dragwyb\Form_Builder\Includes\Toolbars;
 use Dragwyb\Form_Builder\Includes\Toolbars\Toolbar_Base;
 use Dragwyb\Form_Builder\Includes\Modules\Modules;
 
-class Register_Toolbar {
+class Register_Toolbar
+{
 
     private static ?Register_Toolbar $instance = null;
 
@@ -14,19 +16,21 @@ class Register_Toolbar {
 
     private array $default_toolbars = [
         'modules',
-        'general_settings',
+        'style_settings',
+        'after_submission',
         'advance_settings',
-        'after_submission'
     ];
 
-    public static function instance(): self {
+    public static function instance(): self
+    {
         if (self::$instance === null) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->register_default_toolbars();
 
         // Hook to allow developers to add custom toolbars
@@ -36,11 +40,12 @@ class Register_Toolbar {
     /**
      * Register core toolbars (Add Field, General, Advance)
      */
-    private function register_default_toolbars(): void {
+    private function register_default_toolbars(): void
+    {
         foreach ($this->default_toolbars as $toolbar) {
             $dir   = dirname(__NAMESPACE__);
 
-            $class = $dir . '\\'.ucfirst($toolbar).'\\' . ucfirst($toolbar);
+            $class = $dir . '\\' . ucfirst($toolbar) . '\\' . ucfirst($toolbar);
 
             if (class_exists($class)) {
                 $this->register_toolbar(new $class());
@@ -51,21 +56,24 @@ class Register_Toolbar {
     /**
      * Register a single toolbar
      */
-    public function register_toolbar(Toolbar_Base $toolbar): void {
+    public function register_toolbar(Toolbar_Base $toolbar): void
+    {
         $this->toolbars[$toolbar->get_toolbar_id()] = $toolbar;
     }
 
     /**
      * Get all registered toolbars
      */
-    public function get_toolbars(): array {
+    public function get_toolbars(): array
+    {
         return $this->toolbars;
     }
 
     /**
      * Get a specific toolbar
      */
-    public function get_toolbar(string $id): ?Toolbar_Base {
+    public function get_toolbar(string $id): ?Toolbar_Base
+    {
         return $this->toolbars[$id] ?? null;
     }
 }
