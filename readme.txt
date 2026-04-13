31f25acd9b41a6b7e15482005837bf5322eb12b8
dragwyb <dragwyb@gmail.com>
2026-04-13 21:39:35 +0530

    minor update.
    minor update.

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: dragwyb, aniketji007
 Tags: form builder, contact form, custom forms, drag and drop
 Requires at least: 5.8
 Tested up to: 6.9
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- smart-form-builder-by-dragwyb.php
+++ smart-form-builder-by-dragwyb.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Smart Form Builder by Dragwyb
  * Description: Drag and drop form builder for WordPress
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: dragwyb
  * Author URI:  https://dragwyb.com/
  * Text Domain: smart-form-builder-by-dragwyb
@@ -25,7 +25,7 @@ final class Dragwyb_Form_Builder
     /**
      * Plugin version
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * Plugin instance
