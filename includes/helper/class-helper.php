d4820d603545138225ee2866008127b5c2715e9c
dragwyb <dragwyb@gmail.com>
2026-04-19 19:36:40 +0530

    Added new methods in helper file.
    Added new methods in help for convert Namespace into path and path into namespace.

--- includes/helper/class-helper.php
+++ includes/helper/class-helper.php
@@ -17,4 +17,24 @@ class Helper
         $dir = (strpos(DRAGWYB_FORM_BUILDER_PATH, '\\') !== false) ? '\\' : '/';
         return $dir;
     }
+
+    public static function namespace_into_dir_path($namespace)
+    {
+        $seperator = self::directory_separator();
+        if (!strpos($namespace, $seperator)) {
+            $namespace = str_replace('\\', $seperator, $namespace);
+        }
+        return $namespace;
+    }
+
+    public static function dir_path_into_namespace($dir)
+    {
+        $seperator = self::directory_separator();
+
+        if (strpos($dir, $seperator)) {
+            $dir = str_replace($seperator, '\\', $dir);
+        }
+
+        return $dir;
+    }
 }
