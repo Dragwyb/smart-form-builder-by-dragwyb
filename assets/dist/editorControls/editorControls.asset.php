1314ba51203f02942a08b115bbc5b4ed94da5703
dragwyb <dragwyb@gmail.com>
2026-05-10 15:59:01 +0530

    Improve icon control styling.
    Improve icon control styling.

--- assets/dist/editorControls/editorControls.asset.php
+++ assets/dist/editorControls/editorControls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '21a1d5b4685c9fc41aab');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '8377750eb13c501f4c5f');
