47ea9fda1e018da11cf56ec9ed93f37cbaa664f8
dragwyb <dragwyb@gmail.com>
2026-05-12 00:03:47 +0530

    minor style improvement.
    minor style improvement.

--- assets/dist/editor/editor.asset.php
+++ assets/dist/editor/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'e2486a799ce8c5efaf1d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'aa5800d0cfc2e9366c1d');
