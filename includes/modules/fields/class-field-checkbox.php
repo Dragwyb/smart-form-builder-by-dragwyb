dde06ed54db5efb2a525b1a29034d21e578a605d
dragwyb <dragwyb@gmail.com>
2026-05-24 01:33:19 +0530

    Improve checkbox field attribute rendering.
    Improve checkbox field attribute rendering.

--- includes/modules/fields/class-field-checkbox.php
+++ includes/modules/fields/class-field-checkbox.php
@@ -223,8 +223,16 @@ class Field_Checkbox extends Field_Base {
 
 		$layout_class = ( $layout === 'inline' ) ? 'dragwyb-inline-options' : '';
 
+		$this->add_field_attributes(
+			'wrapper',
+			array(
+				'id'    => $this->field_wrapper_id( $id ),
+				'class' => $this->field_wrapper_class( $classes ) . ' dragwyb-no-float',
+			)
+		);
+
 		?>
-		<div id="<?php echo esc_attr( $this->field_wrapper_id( $id ) ); ?>" class="<?php echo esc_attr( $this->field_wrapper_class( $classes ) ); ?> dragwyb-no-float">
+		<div <?php $this->render_field_attributes( 'wrapper' ); ?>>
 			<div class="dragwyb-input-group">
 				<?php $this->render_field_label( '', $label, isset( $required ) ? $required : false, $settings ); ?>
 
@@ -237,9 +245,19 @@ class Field_Checkbox extends Field_Base {
 						}
 
 						$label_text = isset( $opt['option_label'] ) ? $opt['option_label'] : '';
+
+						$this->add_field_attributes(
+							"input_{$index}",
+							array(
+								'type'  => 'checkbox',
+								'id'    => $opt_id,
+								'name'  => $field_id . '[]',
+								'value' => $opt['option_value'],
+							)
+						);
 						?>
 						<label class="dragwyb-option-item" for="<?php echo esc_attr( $opt_id ); ?>">
-							<input type="checkbox" id="<?php echo esc_attr( $opt_id ); ?>" name="<?php echo esc_attr( $field_id ); ?>[]" value="<?php echo isset( $opt['option_value'] ) ? esc_attr( $opt['option_value'] ) : ''; ?>">
+							<input <?php $this->render_field_attributes( "input_{$index}" ); ?> />
 							<span class="dragwyb-radio-label"><?php echo esc_html( $label_text ); ?></span>
 						</label>
 					<?php endforeach; ?>
