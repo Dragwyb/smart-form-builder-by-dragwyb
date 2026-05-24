91c51f3e8dbf2aa31b92cf1f595dcf19f7624848
dragwyb <dragwyb@gmail.com>
2026-05-24 17:02:28 +0530

    Sanitize dynamic values.
    Sanitize dynamic values.

--- includes/after-submission/actions/class-save-submissions-action.php
+++ includes/after-submission/actions/class-save-submissions-action.php
@@ -76,11 +76,11 @@ class Save_Submissions_Action extends Action_Base {
 		$ip = '127.0.0.1';
 
 		if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
-			$ip = $_SERVER['HTTP_CLIENT_IP'];
+			$ip = sanitize_text_field( wp_unslash( $_SERVER['HTTP_CLIENT_IP'] ) );
 		} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			$ip = sanitize_text_field( wp_unslash( $_SERVER['HTTP_X_FORWARDED_FOR'] ) );
 		} elseif ( ! empty( $_SERVER['REMOTE_ADDR'] ) ) {
-			$ip = $_SERVER['REMOTE_ADDR'];
+			$ip = sanitize_text_field( wp_unslash( $_SERVER['REMOTE_ADDR'] ) );
 		}
 
 		// Handle multiple IPs in X-Forwarded-For
