cb29ab61265b6538b6c8511cf922256ceb85e998
dragwyb <dragwyb@gmail.com>
2026-05-25 23:20:13 +0530

    optimize DnD-kit setup and fix input click usability
    - Refactored sensors in DndContext to use a single PointerSensor instead of Mouse/Touch sensors.
    - Added `{ distance: 8 }` activationConstraint to the PointerSensor to prevent drag initiation on standard input clicks.
    - Integrated KeyboardSensor with sortableKeyboardCoordinates to improve accessibility.
    - Switched fallback collision detection algorithm to closestCenter for smoother sorting transitions.
    - Ensured form list mutations are strictly performed on drag completion to prevent unwanted re-renders.

--- assets/dist/editor/editor.asset.php
+++ assets/dist/editor/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '8be16f9d86267c594662');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '6b0ea61469e2eddb2056');
