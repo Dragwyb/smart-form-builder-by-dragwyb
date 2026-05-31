8d8881dd8cea7a7c908b781891651ad7c2583003
dragwyb <dragwyb@gmail.com>
2026-05-31 18:06:27 +0530

    Added is_preview_mode & is_editor_preview_mode method in Helper file.
    Added is_preview_mode & is_editor_preview_mode method in Helper file.

--- includes/helper/class-helper.php
+++ includes/helper/class-helper.php
@@ -6,6 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+use Dragwyb\Form_Builder\Admin\Dragwyb_Pages\Dragwyb_Post;
+
 class Helper {
 
 	public static function directory_separator() {
@@ -33,5 +35,38 @@ class Helper {
 		}
 
 		return $dir;
+	}
+
+	final public static function is_preview_mode(): bool {
+		$form_preview_id = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : '';
+		$form_id         = isset( $_GET['p'] ) ? absint( $_GET['p'] ) : 0;
+		$post_type       = isset( $_GET['post_type'] ) ? sanitize_text_field( wp_unslash( $_GET['post_type'] ) ) : '';
+
+		if ( $form_preview_id && $form_id && $post_type === Dragwyb_Post::POST_TYPE && self::current_user_can_preview( $form_id ) && wp_verify_nonce( $form_preview_id, self::preview_private_key_name( $form_id ) ) && is_user_logged_in() ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static function current_user_can_preview( int $form_id ): bool {
+		return current_user_can( 'manage_options' ) || current_user_can( 'edit_post', $form_id );
+	}
+
+	private static function preview_private_key_name( int $form_id ): string {
+		$post_type = Dragwyb_Post::POST_TYPE;
+		return $post_type . '-' . $form_id;
+	}
+
+	final public static function is_editor_preview_mode(): bool {
+		if ( self::is_preview_mode() ) {
+			$iframe_mode = isset( $_GET['dragwyb_iframe_mode'] ) ? sanitize_key( wp_unslash( $_GET['dragwyb_iframe_mode'] ) ) : '';
+
+			if ( 'true' === $iframe_mode ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
