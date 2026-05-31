f049e18ec8871a97cf818669e38637f2e1666875
dragwyb <dragwyb@gmail.com>
2026-06-01 00:48:35 +0530

    Fixed IconsManager not render Icon on editor preview iframe.
    Fixed IconsManager not render Icon on editor preview iframe.

--- assets/dist/editor/editor.asset.php
+++ assets/dist/editor/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '38577656b379139c9440');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '09e9510dc98e26b27253');
