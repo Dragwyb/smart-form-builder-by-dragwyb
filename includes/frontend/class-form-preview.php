6712ac9d67ede30ddf6236d1f1d70c044cfd9a7d
dragwyb <dragwyb@gmail.com>
2026-05-31 18:16:12 +0530

    Enqueue and register fieldBase in form preview field instead of row field.
    Enqueue and register fieldBase in form preview field instead of row field.
    Enqueue editorPreview assets file.

--- includes/frontend/class-form-preview.php
+++ includes/frontend/class-form-preview.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Dragwyb\Form_Builder\Includes\Frontend;
 
 use Dragwyb\Form_Builder\Admin\Dragwyb_Pages\Dragwyb_Post;
+use Dragwyb\Form_Builder\Includes\Helper\Helper;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -36,73 +37,168 @@ class Form_Preview {
 			return;
 		}
 
-		$form_preview_id = isset( $_GET['preview_id'] ) ? sanitize_text_field( wp_unslash( $_GET['preview_id'] ) ) : '';
-		$form_id         = isset( $_GET['p'] ) ? absint( $_GET['p'] ) : 0;
-		$post_type       = isset( $_GET['post_type'] ) ? sanitize_text_field( wp_unslash( $_GET['post_type'] ) ) : '';
+		if ( ! Helper::is_preview_mode() ) {
+			return;
+		}
 
-		if ( $form_preview_id && $form_id && $post_type === Dragwyb_Post::POST_TYPE && $this->current_user_can_preview( $form_id ) && wp_verify_nonce( $form_preview_id, self::private_key_name( $form_id ) ) ) {
+		if ( function_exists( 'status_header' ) ) {
+			status_header( 200 );
+		}
 
-			if ( function_exists( 'status_header' ) ) {
-				status_header( 200 );
-			}
+		$form_id = isset( $_GET['p'] ) ? absint( $_GET['p'] ) : 0;
 
-			$iframe_mode = isset( $_GET['dragwyb_iframe_mode'] ) ? sanitize_key( wp_unslash( $_GET['dragwyb_iframe_mode'] ) ) : '';
+		if ( Helper::is_editor_preview_mode() ) {
 
-			if ( 'true' === $iframe_mode ) {
-				do_action( 'Dragwyb/Editor/Preview/Init' );
-				$frontend_render      = Frontend_Render::instance();
-				self::$form_id        = $form_id;
-				self::$is_iframe_mode = true;
-				$frontend_render->init( self::$form_id );
-				$frontend_render->enqueue_static_assets();
-				$this->enqueue_editor_preview_styles();
-				$this->enqueue_editor_preview_scripts();
-                // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- it's a core WordPress hook for rendering the head section
-				do_action( 'wp_head' );
-				exit;
-			}
+			add_filter( 'show_admin_bar', '__return_false' );
 
-			$post_id = $form_id;
+			// Remove all WordPress actions
+			remove_all_actions( 'wp_head' );
+			remove_all_actions( 'wp_print_styles' );
+			remove_all_actions( 'wp_print_head_scripts' );
+			remove_all_actions( 'wp_footer' );
 
-			! defined( 'DRAGWYB_FORM_PREVIEW' ) && define( 'DRAGWYB_FORM_PREVIEW', true );
+			// Handle `wp_head`
+			add_action( 'wp_head', 'wp_enqueue_scripts', 1 );
+			add_action( 'wp_head', 'wp_print_styles', 8 );
+			add_action( 'wp_head', 'wp_print_head_scripts', 9 );
+			add_action( 'wp_head', 'wp_site_icon' );
+			add_action( 'wp_head', array( $this, 'editor_head_trigger' ), 30 );
 
-			add_filter( 'pre_get_document_title', array( $this, 'set_document_title' ), 999 );
+			// Handle `wp_footer`
+			add_action( 'wp_footer', 'wp_print_footer_scripts', 20 );
+			add_action( 'wp_footer', 'wp_auth_check_html', 30 );
+			add_action( 'wp_footer', array( $this, 'wp_footer' ) );
 
-			if ( function_exists( 'get_header' ) ) {
-				get_header();
-			}
+			// Handle `wp_enqueue_scripts`
+			remove_all_actions( 'wp_enqueue_scripts' );
 
-			// 3. ECHO THE SHORTCODE (Crucial Step)
-			echo '<div id="dragwyb-preview-wrapper" style="width: 100%;">';
-			echo do_shortcode( '[dragwyb-form id="' . $post_id . '"]' );
-			echo '</div>';
+			// Also remove all scripts hooked into after_wp_tiny_mce.
+			remove_all_actions( 'after_wp_tiny_mce' );
 
-			if ( function_exists( 'get_footer' ) ) {
-				get_footer();
-			}
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 999999 );
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ), 999999 );
 
-			remove_filter( 'pre_get_document_title', array( $this, 'set_document_title' ), 999 );
-
-			// 4. STOP EXECUTION
+			// Setup default heartbeat options
+			// add_filter(
+			// 'heartbeat_settings',
+			// function ( $settings ) {
+			// $settings['interval'] = 15;
+			// return $settings;
+			// }
+			// );
+			do_action( 'Dragwyb/Editor/Preview/Init' );
+			$frontend_render      = Frontend_Render::instance();
+			self::$form_id        = $form_id;
+			self::$is_iframe_mode = true;
+			$frontend_render->init( self::$form_id );
+			$frontend_render->enqueue_static_assets();
+			$frontend_render->get_toolbar_data( 'fields' );
+			wp_head();
+			$this->enqueue_editor_preview_styles();
+			$this->enqueue_editor_preview_scripts();
+			// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- it's a core WordPress hook for rendering the head section
+			// do_action( 'wp_head' );
+			// do_action( 'wp_footer' );
+			wp_footer();
 			exit;
 		}
+
+		$post_id = $form_id;
+
+		! defined( 'DRAGWYB_FORM_PREVIEW' ) && define( 'DRAGWYB_FORM_PREVIEW', true );
+
+		add_filter( 'pre_get_document_title', array( $this, 'set_document_title' ), 999 );
+
+		if ( function_exists( 'get_header' ) ) {
+			get_header();
+		}
+
+		// 3. ECHO THE SHORTCODE (Crucial Step)
+		echo '<div id="dragwyb-preview-wrapper" style="width: 100%;">';
+		echo do_shortcode( '[dragwyb-form id="' . $post_id . '"]' );
+		echo '</div>';
+
+		if ( function_exists( 'get_footer' ) ) {
+			get_footer();
+		}
+
+		remove_filter( 'pre_get_document_title', array( $this, 'set_document_title' ), 999 );
+
+		// 4. STOP EXECUTION
+		exit;
 	}
 
 	public function init_iframe() {}
 
 	public function enqueue_editor_preview_styles() {
+		$css_assets_info = array(
+			'version'      => DRAGWYB_FORM_BUILDER_VERSION,
+			'dependencies' => array(
+				'wp-auth-check',
+			),
+		);
+
 		wp_enqueue_style(
 			'dragwyb-form-editor-global',
 			esc_url( DRAGWYB_FORM_BUILDER_URL . 'assets/css/editor-global.css' ),
-			array(),
-			esc_attr( DRAGWYB_FORM_BUILDER_VERSION )
+			$css_assets_info['dependencies'],
+			$css_assets_info['version']
 		);
 
 		wp_enqueue_style( 'dragwyb-editor-preview', esc_url( DRAGWYB_FORM_BUILDER_URL . '/assets/css/editor-preview.css' ), array( 'dragwyb-form-editor-global' ), esc_attr( DRAGWYB_FORM_BUILDER_VERSION ) );
 	}
 
 	public function enqueue_editor_preview_scripts() {
-		do_action( 'Dragwyb/Editor/Preview/Enqueue_Scripts' );
+			$js_assets_info = array(
+				'version'      => DRAGWYB_FORM_BUILDER_VERSION,
+				'dependencies' => array(
+					'dragwyb_editor_preview',
+				),
+			);
+
+			$js_editor_preview_assets_info = array(
+				'version'      => DRAGWYB_FORM_BUILDER_VERSION,
+				'dependencies' => array(
+					'dragwyb-form-core',
+					'wp-auth-check',
+					'heartbeat',
+					'react',
+					'react-dom',
+				),
+			);
+
+			if ( file_exists( DRAGWYB_FORM_BUILDER_PATH . 'assets/dist/editorFields/editorFields.asset.php' ) ) {
+				$dragwyb_js_assets_info = require_once DRAGWYB_FORM_BUILDER_PATH . 'assets/dist/editorFields/editorFields.asset.php';
+
+				if ( isset( $dragwyb_js_assets_info['dependencies'] ) ) {
+					$js_assets_info['dependencies'] = array_merge( $js_assets_info['dependencies'], $dragwyb_js_assets_info['dependencies'] );
+				}
+
+				if ( isset( $dragwyb_js_assets_info['version'] ) ) {
+					$js_assets_info['version'] = $dragwyb_js_assets_info['version'];
+				}
+			}
+
+			if ( file_exists( DRAGWYB_FORM_BUILDER_PATH . 'assets/dist/editorPreview/editorPreview.asset.php' ) ) {
+				$dragwyb_js_assets_info = require_once DRAGWYB_FORM_BUILDER_PATH . 'assets/dist/editorPreview/editorPreview.asset.php';
+
+				if ( isset( $dragwyb_js_assets_info['dependencies'] ) ) {
+					$js_editor_preview_assets_info['dependencies'] = array_merge( $js_editor_preview_assets_info['dependencies'], $dragwyb_js_assets_info['dependencies'] );
+				}
+
+				if ( isset( $dragwyb_js_assets_info['version'] ) ) {
+					$js_editor_preview_assets_info['version'] = $dragwyb_js_assets_info['version'];
+				}
+			}
+
+			wp_register_script( 'dragwyb_editor_preview', esc_url( DRAGWYB_FORM_BUILDER_URL . 'assets/dist/editorPreview/editorPreview.js' ), $js_editor_preview_assets_info['dependencies'], esc_attr( $js_editor_preview_assets_info['version'] ), true );
+
+			wp_register_script( 'dragwyb_editor_fields', esc_url( DRAGWYB_FORM_BUILDER_URL . 'assets/dist/editorFields/editorFields.js' ), $js_assets_info['dependencies'], esc_attr( $js_assets_info['version'] ), true );
+
+			wp_enqueue_script( 'dragwyb_editor_preview' );
+			wp_enqueue_script( 'dragwyb_editor_fields' );
+
+			do_action( 'Dragwyb/Editor/Preview/Enqueue_Scripts' );
 	}
 
 	public function set_document_title( string $title ): string {
