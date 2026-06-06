da9d32847d6c957c6b67be71fca498ae5b305cad
dragwyb <dragwyb@gmail.com>
2026-06-06 13:26:22 +0530

    Improved editor setting panel styling.
    Improved editor setting panel styling.

--- assets/dist/editor/editor.asset.php
+++ assets/dist/editor/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '09e9510dc98e26b27253');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'b6dbd81a2506b34487c8');
