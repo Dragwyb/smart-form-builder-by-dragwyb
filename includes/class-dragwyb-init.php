15bc4b0a4b63c6c6e7a553c26b807632e39b77cd
dragwyb <dragwyb@gmail.com>
2026-06-13 15:31:58 +0530

    Load review notice.
    Load review notice.

--- includes/class-dragwyb-init.php
+++ includes/class-dragwyb-init.php
@@ -20,6 +20,7 @@ use Dragwyb\Form_Builder\Includes\Frontend\Managers\CSS_Manager;
 use Dragwyb\Form_Builder\Includes\Rest_Routes\Dragwyb_Frontend_Route;
 use Dragwyb\Form_Builder\Includes\Rest_Routes\Dragwyb_Settings_Route;
 use Dragwyb\Form_Builder\Admin\Feedback\SMFBD_Feedback_Form;
+use Dragwyb\Form_Builder\Admin\Review\Dragwyb_Review_Notice;
 
 class Dragwyb_Init {
 
@@ -43,6 +44,8 @@ class Dragwyb_Init {
 			Dragwyb_Settings::instance();
 			Frontend_Render::instance();
 			SMFBD_Feedback_Form::get_instance();
+
+			$this->review_notice();
 		}
 
 		new Dragwyb_Frontend_Route();
@@ -117,6 +120,13 @@ class Dragwyb_Init {
 			esc_attr( DRAGWYB_FORM_BUILDER_VERSION ),
 			'all'
 		);
+	}
+
+	private function review_notice() {
+		$already_rated = get_option( 'dragwyb_form_builder_already_reviewd', false );
+		if ( ! $already_rated && class_exists( Dragwyb_Review_Notice::class ) ) {
+			Dragwyb_Review_Notice::instance();
+		}
 	}
 
 	private function get_translations() {
