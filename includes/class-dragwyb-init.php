fcf4ff7ef941c22776f2fa5d6dfd981bc22ca332
dragwyb <dragwyb@gmail.com>
2026-06-13 12:30:33 +0530

    load feedback form files.
    load feedback form files.

--- includes/class-dragwyb-init.php
+++ includes/class-dragwyb-init.php
@@ -19,6 +19,7 @@ use Dragwyb\Form_Builder\Includes\Frontend\Form_Preview;
 use Dragwyb\Form_Builder\Includes\Frontend\Managers\CSS_Manager;
 use Dragwyb\Form_Builder\Includes\Rest_Routes\Dragwyb_Frontend_Route;
 use Dragwyb\Form_Builder\Includes\Rest_Routes\Dragwyb_Settings_Route;
+use Dragwyb\Form_Builder\Admin\Feedback\SMFBD_Feedback_Form;
 
 class Dragwyb_Init {
 
@@ -41,6 +42,7 @@ class Dragwyb_Init {
 			new Dragwyb_Form_Builder_Ajax();
 			Dragwyb_Settings::instance();
 			Frontend_Render::instance();
+			SMFBD_Feedback_Form::get_instance();
 		}
 
 		new Dragwyb_Frontend_Route();
