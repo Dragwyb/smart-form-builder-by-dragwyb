64987deee542efcba03bb21919acb80b5f20a6bb
dragwyb <dragwyb@gmail.com>
2026-06-22 23:16:51 +0530

    Improve editor controls styling.
    Improve editor select options control & repeater add button styling.

--- assets/dist/editor/editor.asset.php
+++ assets/dist/editor/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '59288d59f0cc7cfd83f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '28860102f96c1f6fe665');
--- assets/dist/editorControls/editorControls.asset.php
+++ assets/dist/editorControls/editorControls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '7b5c26ec250da5211e32');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '813ae9d2e7f02d06d5ae');
