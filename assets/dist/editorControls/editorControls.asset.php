96671729c0336f00b27d44f5c74525cb4a682a19
dragwyb <dragwyb@gmail.com>
2026-07-10 20:52:07 +0530

    Improve heading control styling.
    Improve heading control styling.

--- assets/dist/editorControls/editorControls.asset.php
+++ assets/dist/editorControls/editorControls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '99e5d5061b848a17a3ce');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'c3375899200e9f37c97f');
