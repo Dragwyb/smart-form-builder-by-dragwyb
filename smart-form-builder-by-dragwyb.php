c41e9b52c73d0516fe9212db0da0e0f60c9fd352
dragwyb <dragwyb@gmail.com>
2026-08-20 22:04:18 +0530

    Added plugin action links.
    Added plugin action links.

--- smart-form-builder-by-dragwyb.php
+++ smart-form-builder-by-dragwyb.php
@@ -66,8 +66,9 @@ final class Dragwyb_Form_Builder {
 		define( 'DRAGWYB_PREFIX', 'dragwyb' );
 		define( 'DRAGWYB_TEXT_DOMAIN', 'smart-form-builder-by-dragwyb' );
 		define( 'DRAGWYB_FORM_BUILDER_VERSION', self::VERSION );
-		define( 'DRAGWYB_FORM_BUILDER_PATH', plugin_dir_path( __FILE__ ) );
-		define( 'DRAGWYB_FORM_BUILDER_URL', plugin_dir_url( __FILE__ ) );
+		define( 'DRAGWYB_FORM_BUILDER_FILE', __FILE__ );
+		define( 'DRAGWYB_FORM_BUILDER_PATH', plugin_dir_path( DRAGWYB_FORM_BUILDER_FILE ) );
+		define( 'DRAGWYB_FORM_BUILDER_URL', plugin_dir_url( DRAGWYB_FORM_BUILDER_FILE ) );
 	}
 
 	/**
