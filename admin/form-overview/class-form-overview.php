8ab0baf1f6ff72b2420657678874dc79be44f35b
dragwyb <dragwyb@gmail.com>
2026-08-20 17:58:13 +0530

    Improve add form button styling in overview page.
    Improve add form button styling in overview page.

--- admin/form-overview/class-form-overview.php
+++ admin/form-overview/class-form-overview.php
@@ -92,7 +92,7 @@ if ( ! class_exists( 'Form_Overview' ) ) {
 					<div class="dragwyb-header-right">
 						<a href="<?php echo esc_url( admin_url( 'admin.php?page=' . DRAGWYB_PREFIX . '-form-builder' ) ); ?>" class="dragwyb-btn-create">
 							<span class="dashicons dashicons-plus-alt2"></span>
-							<?php esc_html_e( 'Create New Form', 'smart-form-builder-by-dragwyb' ); ?>
+							<?php esc_html_e( 'Add Form', 'smart-form-builder-by-dragwyb' ); ?>
 						</a>
 					</div>
 				</div>
