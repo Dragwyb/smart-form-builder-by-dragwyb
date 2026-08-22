581ffb80c6e99dce5c5240fa798dec51450e839e
dragwyb <dragwyb@gmail.com>
2026-08-22 20:32:20 +0530

    Update form preview edit form title.
    Update form preview edit form title.

--- includes/frontend/class-form-preview.php
+++ includes/frontend/class-form-preview.php
@@ -154,7 +154,7 @@ class Form_Preview {
 		$editor_url = admin_url( 'admin.php?page=' . DRAGWYB_PREFIX . '-form-builder&form_id=' . $form_id );
 		$logo_url   = DRAGWYB_FORM_BUILDER_URL . 'assets/img/menu-logo.svg';
 		$logo_html  = '<img src="' . esc_url( $logo_url ) . '" style="width: 16px; height: 16px; vertical-align: middle; margin-right: 6px; margin-top: -2px;" />';
-		$title      = $logo_html . __( 'Smart Form', 'smart-form-builder-by-dragwyb' );
+		$title      = $logo_html . __( 'Edit Form', 'smart-form-builder-by-dragwyb' );
 
 		$wp_admin_bar->add_node(
 			array(
